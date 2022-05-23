4f3da3f26db3b658c3ac88af3f9227b09572c787
Mickey <mickeyc11111@gmail.com>
2022-05-23 18:37:07 -0400

    commit tree 58a818decff558eaf194b66c2387aa3ced749a05

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching (array, name){
+    let match = array.filter((key) => key.toUpperCase() === name.toUpperCase());
+    return match;
+}
+
+function fuzzyMatch (array, name) {
+    let newArray = [];
+    array.filter((key) => {
+      if (key.startsWith(name)){
+        newArray.push(key)
+      }
+    })
+    return newArray
+  }
+  
+function matchName (array, string) {
+    let match = [];
+      array.filter(element => {
+          if (element.name === string){
+              match.push(element)
+          }
+      })
+    return match;
+  }
